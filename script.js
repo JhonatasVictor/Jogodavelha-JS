997b2e5ca44242253ef3f0552d49646b18ce7a78
JhonatasVictor <152637785+JhonatasVictor@users.noreply.github.com>
2024-04-09 17:03:29 -0300

    atualizção

---
+++ script.js
@@ -0,0 +1,15 @@
+   let jogador = 'X'        
+    function jogar(celula) {
+        if (celula.innerHTML == '') {
+            celula.innerHTML = jogador
+        }
+
+            if (jogador == 'X') {
+                jogador = 'O'
+            } else {
+                jogador = 'X'
+            }
+    }
+    function reiniciar() {
+        location.reload()
+    }
